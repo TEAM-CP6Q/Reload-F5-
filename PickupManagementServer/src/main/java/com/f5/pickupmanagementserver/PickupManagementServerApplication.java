e322563abb6eb343542e33f87eb0fa58700d7561
DonggyunHan <hdg5639@daum.net>
2024-10-05 20:40:45 +0900

    First Commit

---
+++ PickupManagementServer/src/main/java/com/f5/pickupmanagementserver/PickupManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.f5.pickupmanagementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickupManagementServerApplication.class, args);
+    }
+
+}
