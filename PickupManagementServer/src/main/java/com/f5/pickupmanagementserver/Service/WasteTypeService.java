98a77c6991bab3ed579588d021088f299dbe4c24
DonggyunHan <hdg5639@naver.com>
2024-11-10 14:49:27 +0900

    Revert "수거 신청, 삭제, 수정, 전체 목록 불러오기, 유저 신청 목록 불러오기, 신청 상세내역 불러오기 완성"
    This reverts commit 26935109
    
    Signed-off-by: donggyun-han <hdg5639@daum.net>

--- PickupManagementServer/src/main/java/com/f5/pickupmanagementserver/Service/WasteTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.f5.pickupmanagementserver.Service;
-
-import com.f5.pickupmanagementserver.DTO.WasteTypeDTO;
-
-import java.util.List;
-
-public interface WasteTypeService {
-    List<WasteTypeDTO> getWasteList(String fileName);
-}
