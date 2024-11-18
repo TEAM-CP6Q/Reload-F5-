d0b76de57a86976e436e59436f85357ea8dbe079
dongsubnambuk <ehdtjq1278@naver.com>
2024-11-18 12:50:44 +0900

    cide 파일수정 4

--- reload-front/deploy.sh
+++ reload-front/deploy.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+
 REPOSITORY=/home/ubuntu/deploy
+BUILD_DIR=/home/ubuntu/deploy/build
 
-cd $REPOSITORY 
+# 이동
+cd $REPOSITORY
 
-sudo npm install
+# 의존성 설치
+npm ci --only=production
 
-sudo pm2 reload all
+# PM2로 애플리케이션 실행/재시작
+if pm2 list | grep -q "F5"; then
+  pm2 reload F5
+else
+  pm2 start npx --name F5 serve -s $BUILD_DIR -l 3000
+fi
+
+# PM2 상태 저장
+pm2 save
