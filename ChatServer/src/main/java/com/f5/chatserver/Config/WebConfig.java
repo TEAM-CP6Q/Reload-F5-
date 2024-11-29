3e2bf5248044c185234c00fe17068c865d8df0a1
donggyun-han <hdg5639@daum.net>
2024-11-29 20:26:58 +0900

    CORS 수정
    Signed-off-by: donggyun-han <hdg5639@daum.net>

--- ChatServer/src/main/java/com/f5/chatserver/Config/WebConfig.java
+++ ChatServer/src/main/java/com/f5/chatserver/Config/WebConfig.java
@@ -1,28 +1,28 @@
-package com.f5.chatserver.Config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                        .allowedOrigins("http://127.0.0.1:3000",
-                                "http://3.37.122.192:8000",
-                                "http://localhost:8000",
-                                "https://refresh-f5.store",
-                                "http://15.165.174.146:3000") // 모든 도메인 허용
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+//package com.f5.chatserver.Config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
+//                        .allowedOrigins("http://127.0.0.1:3000",
+//                                "http://3.37.122.192:8000",
+//                                "http://localhost:8000",
+//                                "https://refresh-f5.store",
+//                                "http://15.165.174.146:3000") // 모든 도메인 허용
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+//}
