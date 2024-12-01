8bb811c68b4990194f83c6c14181dce1892b2157
ganglike <rudfkr248@gmail.com>
2024-12-02 03:20:23 +0900

    검색 클래스이름 수정

--- reload-front/src/pages/SearhResultsPage.jsx
+++ reload-front/src/pages/SearhResultsPage.jsx
@@ -19,21 +19,21 @@ const SearchResultsPage = () => {
 
         return (
             <div 
-                className={`main-product-card ${isPressed ? 'card-pressed' : ''}`}
+                className={`search-results-card ${isPressed ? 'search-results-card-pressed' : ''}`}
                 onClick={handleClick}
                 onMouseDown={() => setIsPressed(true)}
                 onMouseUp={() => setIsPressed(false)}
                 onMouseLeave={() => setIsPressed(false)}
             >
-                <div className="main-product-card-image-container">
-                    <div className="new-product-card">
-                        <img src={product.imageUrls[0]} alt={product.name} className="new-product-image" />
+                <div className="search-results-image-container">
+                    <div className="search-results-card-wrapper">
+                        <img src={product.imageUrls[0]} alt={product.name} className="search-results-image" />
                     </div>
                 </div>
-                <div className="main-product-card-content">
-                    <div className="new-product-designer">{product.designer?.name || '브랜드명'}</div>
-                    <div className="new-product-name">{product.name}</div>
-                    <div className="new-product-price">{product.price?.toLocaleString()}원</div>
+                <div className="search-results-content">
+                    <div className="search-results-designer">{product.designer?.name || '브랜드명'}</div>
+                    <div className="search-results-name">{product.name}</div>
+                    <div className="search-results-price">{product.price?.toLocaleString()}원</div>
                 </div>
             </div>
         );
@@ -43,12 +43,12 @@ const SearchResultsPage = () => {
         <>
             <Header />
             <div className="search-results-container">
-                <div className="search-term-result">
+                <div className="search-results-term">
                     '{searchQuery}' 검색 결과
                 </div>
-                <div className="main-new-products-container">
-                    <div className="main-products-subContanier">
-                        <div className="main-new-products-grid">
+                <div className="search-results-products-container">
+                    <div className="search-results-subcontainer">
+                        <div className="search-results-grid">
                             {searchResults.map((product) => (
                                 <ProductCard key={product.pid} product={product} />
                             ))}
