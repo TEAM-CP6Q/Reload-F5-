4772445d1d18c8dd4281474150b85b355879d5d6
donggyun-han <hdg5639@daum.net>
2024-12-03 11:57:31 +0900

    cicd default test

--- CICDProject/src/main/java/com/f5/cicdproject/Controller/cicdController.java
+++ CICDProject/src/main/java/com/f5/cicdproject/Controller/cicdController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class cicdController {
     @GetMapping("/")
     public String homePage(HttpServletRequest request){
-        return "home";
+        return "default";
     }
 }
