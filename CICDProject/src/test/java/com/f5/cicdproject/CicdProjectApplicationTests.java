3f5ed31b66a1ab8bd364f2e81dd83e14316f3753
donggyun-han <hdg5639@daum.net>
2024-12-03 11:50:40 +0900

    cicd first commit

---
+++ CICDProject/src/test/java/com/f5/cicdproject/CicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5.cicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
